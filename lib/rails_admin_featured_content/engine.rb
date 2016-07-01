c298e360bf93ff5d7490d9e3c080be20af120113
gabrielmedina <gmedina.santos@gmail.com>
2016-07-01 15:57:43 -0400

    Update in structure, styles and scripts

--- lib/rails_admin_featured_content/engine.rb
+++ lib/rails_admin_featured_content/engine.rb
@@ -5,6 +5,7 @@ module RailsAdminFeaturedContent
     isolate_namespace RailsAdminFeaturedContent
 
     initializer 'rails_admin_featured_content.load_static_assets' do |app|
+      app.middleware.use ::ActionDispatch::Static, "#{root}/vendor"
       app.config.assets.precompile += %w(
         rails_admin/featured_content.js
         rails_admin/featured_content.css
