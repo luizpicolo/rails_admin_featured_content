118cf47dfed68c40fcecb97f588c45f318cc6961
Luiz Picolo <luizpicolo@gmail.com>
2016-07-08 07:42:12 -0400

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  path_url = 'admin/featured_content/:id/search_content'
-  path_method = 'rails_admin_featured_content/featured_content#search_content'
-  get path_url => path_method
+  get_path_url = 'admin/featured_content/:id/search_content'
+  get_path_method = 'rails_admin_featured_content/featured_content#search_content'
+  get get_path_url => get_path_method
+
+  put_path_url = 'admin/featured_content/:id/create_images'
+  put_path_method = 'rails_admin_featured_content/featured_content#create_images'
+  put put_path_url => put_path_method
 end
