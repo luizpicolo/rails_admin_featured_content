349f775ca8651140a2d46c12a44e4c438ed012c6
Luiz Picolo <luizpicolo@gmail.com>
2016-07-12 09:37:11 -0400

    update

--- app/controllers/rails_admin_featured_content/featured_content_controller.rb
+++ app/controllers/rails_admin_featured_content/featured_content_controller.rb
@@ -1,7 +1,7 @@
 module RailsAdminFeaturedContent
   class FeaturedContentController < ::ApplicationController
     def search_content
-      @content_builder = ContentBuilder.search(params[:term])
+      @content_builder = RailsAdminContentBuilder::ContentBuilder.search(params[:term])
       if @content_builder
         render json: @content_builder
       else
