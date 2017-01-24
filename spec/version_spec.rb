cf75be41d71f0d899676493c01a247d29acce7ec
Luiz Picolo <luizpicolo@gmail.com>
2017-01-23 21:13:57 -0300

    Bump version 1.2.0

--- spec/version_spec.rb
+++ spec/version_spec.rb
@@ -2,6 +2,6 @@ require 'spec_helper'
 
 describe RailsAdminFeaturedContent do
   it 'has a version number' do
-    expect(RailsAdminFeaturedContent::VERSION).to eq('1.1.0')
+    expect(RailsAdminFeaturedContent::VERSION).to eq('1.2.0')
   end
 end
